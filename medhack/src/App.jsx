ccbe6dfb0339f45f5f4e031dac44a972805e7030
lucywu026 <lwuu0047@student.monash.edu>
2025-02-16 17:15:50 +1100

    image as background

--- medhack/src/App.jsx
+++ medhack/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 import './App.css'
 import Navbar from './components/Navbar'
+import myimage from './assets/regularEyeExam.jpg'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,12 +10,12 @@ function App() {
   return (
     <>
       <Navbar />
-      <div>
+      <div class='background-image'>
         <card>
           <h1 class='website-name'>
             eyeSpy
           </h1>
-          <img src='./assets/regularEyeExam.jpg' alt="regularEyeExams" className="eye-exam-image" />
+          {/* <img src={myimage} alt="eye" className="eye-exam-image" /> */}
           <p>eyeSpy that you need an eyetest!</p>
           <button>Start Your Eye Test Today</button>
         </card>
